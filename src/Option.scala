dbcb7802b38460a979828e6933ef23e5f580b059
Nico Schlumprecht <snico1995@gmail.com>
2017-05-04 14:14:11 -0500

    more docs

--- src/Option.scala
+++ src/Option.scala
@@ -12,18 +12,21 @@ class Option {
 
   var interestRate = 0.01
 
+  // Returns a copy of this with modified strike
   def strike (p : Double) : Option = {
     val modified = copy()
     modified.strikePrice = p
     modified
   }
 
+  // Returns copy with t subtracted from the expiry
   def time (t : Integer) : Option = {
     val modified = copy()
     modified.daysToExp -= t
     modified
   }
 
+  // Returns a copy with the given expiration.
   def expiration (d : Int) : Option = {
     val modified = copy()
     modified.daysToExp = d
@@ -36,36 +39,42 @@ class Option {
     modified
   }
 
+  // Returns a copy with a modified number of contracts (contracts are for 100 shares)
   def contracts (c : Int) : Option = {
     val modified = copy()
     modified.contracts = c
     modified
   }
 
+  // Returns a copy with modified volatility.
   def volatility (v : Double) : Option = {
     val modified = copy()
     modified.volatility = v
     modified
   }
 
+  // Changes this option from a call to a put or the other way around
   def setIsCall (c : Boolean) : Option = {
     val modified = copy()
     modified.isCall = c
     modified
   }
 
+  // Change the risk free interest rate for calculations.
   def irate (i : Double) : Option = {
     val modified = copy()
     modified.interestRate = i
     modified
   }
 
+  // Change the cost basis for this option
   def cost (c : Double) : Option = {
     val modified = copy()
     modified.costBasis = c
     modified
   }
 
+  // Return a deep copy of this option
   def copy () : Option = {
     val copy = new Option
     copy.ticker = ticker
@@ -87,6 +96,7 @@ class Option {
     modified
   }
 
+  // Calculates the current value of this option based on the given underlying price.
   def calculateValue (underlyingPrice : Double) : Double = {
     require(strikePrice != -1, "Strike price is not set on option: " + toString())
     require(daysToExp != -1, "Days to expiration is not set on option: " + toString())
--- src/SpreadBuilder.scala
+++ src/SpreadBuilder.scala
@@ -1,4 +1,7 @@
 
+// This class represents an intermediate spread, where "long x" or "short x" has been defined, but the option that
+// is bought or sold has not been defined yet. A SpreadBuilder must of followed by: of <Option> to do anything with it.
+
 class SpreadBuilder {
   var count : Integer = 0
 
